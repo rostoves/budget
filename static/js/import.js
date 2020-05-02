b87a0eef73c0429cbbc6b387aedb8e182c39362a
rostoves <rostoves@users.noreply.github.com>
2020-05-02 20:14:39 +0300

    initial static

---
+++ static/js/import.js
@@ -0,0 +1,104 @@
+let mccArray = [];
+
+$(document).ready(function () {
+    $('.importMcc').on('change', markNewMcc);
+    $('.splitImportRowButton').on('click', rowInsertAfter);
+    $('.deleteImportRowButton').on('click', rowDelete);
+    $('.importTableButton').on('click', collectImportArray);
+    loadData('', 'get_mcc', function () {
+        mccArray = this;
+        console.log(mccArray);
+        autocompleteMcc();
+        $('.importMcc').each(markNewMcc);
+    });
+});
+
+
+function markNewMcc() {
+    let _mcc = this.value;
+    let _id = '#'+this.id;
+
+    if (mccArray.indexOf(_mcc) === -1 ) {
+        // console.log('Marked new mcc ' + _id);
+        $(_id).addClass("badge-info");
+    } else {
+        // console.log('Cleaned old mcc ' + _id);
+        $(_id).removeClass("badge-info");
+    }
+}
+
+function autocompleteMcc() {
+    $('.importMcc').autocomplete({
+        source: mccArray
+    });
+}
+
+function rowInsertAfter() {
+    let row = this.id.slice(6);
+    let id = getRandom();
+    let date = "<input class='date' type='text' id='date_" + id + "' value='" + $('#date_'+row)[0].value + "'>";
+    let acc = "<span class='account' id='acc_" + id + "'>" + $('#acc_'+row)[0].innerText + "</span>";
+    let status = "<span class='status' id='status_" + id + "'>" + $('#status_'+row)[0].innerText + "</span>";
+    let op_sum = "<input class='operation_sum' id='op_sum_" + id + "' value='0.00'>";
+    let op_cur = "<span class='operation_cur' id='op_cur_" + id + "'>" + $('#op_cur_'+row)[0].innerText + "</span>";
+    let bar_sum = "<input class='bargain_sum' id='bar_sum_" + id + "' value='0.00'>";
+    let bar_cur = "<span class='bargain_cur' id='bar_cur_" + id + "'>" + $('#bar_cur_'+row)[0].innerText + "</span>";
+    let mcc = "<input class='merchant_code importMcc' type='text' id='mcc_" + id + "' value='" + $('#mcc_'+row)[0].value + "'>";
+    let desc = "<input class='description' id='desc_" + id + "' value='" + $('#desc_'+row)[0].value + "'>";
+    let comment = "<input class='comment' type='text' placeholder='Комментарий'>";
+    let deleteImportRowButton = "<button class='deleteImportRowButton' id='delete_" + id + "'>Удалить</button>";
+    $( $( "#row_" + row) ).after(
+        "<div class='importTableRow' id='row_" + id + "'>"
+        + date + acc + status + op_sum + op_cur + bar_sum + bar_cur + mcc + desc + comment + deleteImportRowButton +
+        "</div>"
+    );
+    autocompleteMcc();
+    $('#mcc_'+ id).on('click', markNewMcc);
+    $('.deleteImportRowButton').on('click', rowDelete);
+}
+
+function rowDelete() {
+    let row = this.id.slice(7);
+    console.log("#row_" + row + " was deleted.");
+    $("#row_" + row).remove();
+}
+
+function collectImportArray() {
+    importTableArray = [];
+    $(".importTableRow").each(function() {
+        let arrayOfThisRow = {};
+        let tableData = $(this).children();
+        for (let i = 0; i < tableData.length; i++) {
+            switch (tableData[i].nodeName) {
+                case "INPUT":
+                    arrayOfThisRow[tableData[i].classList[0]] = tableData[i].value;
+                    break;
+                case "SPAN":
+                    arrayOfThisRow[tableData[i].classList[0]] = tableData[i].innerText;
+                    break;
+            }
+        }
+        arrayOfThisRow['id'] = this.id;
+
+        importTableArray.push(arrayOfThisRow);
+    });
+
+    // console.log(importTableArray);
+
+    sendPOST('', 'import_table', importTableArray, function () {
+        renderResultsTable(this);
+    });
+}
+
+function renderResultsTable(arr) {
+    arr.inserted.forEach(function(item, i, arr) {
+        $("#"+item).remove();
+    });
+    if (arr.not_inserted.length < 1) {
+            console.log('ok');
+    } else {
+        arr.not_inserted.forEach(function (item, i, arr) {
+            $("#" + item).addClass("badge-warning");
+        });
+    }
+}
---
+++ static/js/script.js
@@ -0,0 +1,55 @@
+var csrftoken = getCookie('csrftoken');
+
+function loadData(_url, _action, callback) {
+    console.log(_url, _action);
+    $.ajax({
+        url: _url,
+        type: "POST",
+        dataType: "json",
+        headers: {'X-CSRFToken': csrftoken},
+        data: {
+            action: _action
+        },
+        success: function (data) {
+            callback.call(data);
+        }
+    });
+}
+
+function sendPOST(_url, _action, _data = undefined, callback = undefined) {
+    console.log(_url, _action, _data);
+    $.ajax({
+        url: _url,
+        type: "POST",
+        dataType: "json",
+        headers: {'X-CSRFToken': csrftoken},
+        data: {
+            action: _action,
+            data: JSON.stringify(_data)
+        },
+        success: function (data) {
+            console.log(data);
+            callback.call(data);
+        }
+    });
+}
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function getRandom() {
+    return Math.floor(Math.random() * (1000000 - 100000 + 1)) + 100000;
+}
