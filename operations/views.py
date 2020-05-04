341362093895890dc4639b59d36b1245cacaae39
rostoves <rostoves@users.noreply.github.com>
2020-05-04 15:06:08 +0300

    operations list filters

--- operations/views.py
+++ operations/views.py
@@ -10,19 +10,56 @@ logger = logging.getLogger('django')
 
 class OperationListView(ListView):
     model = o_models.Operation
-    paginate_by = 100
+    paginate_by = 50
+
+    def get_filters(self):
+        # print(self.request.GET)
+        filter_args = {}
+        filter_args['status__in'] = self.request.GET.getlist('status[]') \
+            if len(self.request.GET.getlist('status[]')) > 0 else ['OK']
+        if self.request.GET.get('date_from'):
+            filter_args['date__gte'] = self.request.GET.get('date_from')
+        if self.request.GET.get('date_to'):
+            filter_args['date__lte'] = self.request.GET.get('date_to') + ' 23:59:59'
+        if self.request.GET.get('merchant_code[]') \
+                or self.request.GET.get('category[]') \
+                or self.request.GET.get('type[]'):
+
+            filter_arr = []
+            if self.request.GET.get('category[]'):
+                mcc_cat = get_foreign_objects_by_filter('MerchantCode',
+                                                        {'category__in': self.request.GET.getlist('category[]')})
+                for value in mcc_cat:
+                    filter_arr.append(value)
+
+            if self.request.GET.get('type[]'):
+                cat_type = get_foreign_objects_by_filter('Category',
+                                                         {'type__in': self.request.GET.getlist('type[]')})
+                mcc_cat = get_foreign_objects_by_filter('MerchantCode',
+                                                        {'category__in': cat_type})
+                for value in mcc_cat:
+                    filter_arr.append(value)
+
+            if self.request.GET.get('merchant_code[]'):
+                for value in self.request.GET.getlist('merchant_code[]'):
+                    filter_arr.append(value)
+
+            filter_args['merchant_code__in'] = filter_arr
+
+        print(filter_args)
+        return filter_args
 
     def get_queryset(self):
-        filter_args = {}
-        filter_args['status__in'] = [self.request.GET.get('status[]', 'OK')]
-        if self.request.GET.get('merchant_code[]'):
-            filter_args['merchant_code__in'] = [self.request.GET.get('merchant_code[]')]
+        filter_args = self.get_filters()
 
-        order = self.request.GET.get('orderby', 'date')
-
-        new_context = o_models.Operation.objects.filter(
-            **filter_args
-        ).order_by(order)
+        if self.request.GET.get('orderby') == 'ASC':
+            new_context = o_models.Operation.objects.filter(
+                **filter_args
+            ).order_by('date')
+        else:
+            new_context = o_models.Operation.objects.filter(
+                **filter_args
+            ).order_by('-date')
 
         return new_context
 
@@ -30,7 +67,9 @@ class OperationListView(ListView):
         context = super(OperationListView, self).get_context_data(**kwargs)
         context['status'] = get_column('Operation', 'status')
         context['merchant_code'] = get_row_and_id('MerchantCode', 'name')
-        context['orderby'] = self.request.GET.get('orderby', 'date')
+        context['category'] = get_row_and_id('Category', 'name')
+        context['type'] = get_row_and_id('Type', 'name')
+        context['orderby'] = self.request.GET.get('orderby')
         return context
 
 
@@ -40,11 +79,13 @@ class OperationsView(OperationListView):
     def get(self, request, *args, **kwargs):
         if self.request.GET.get('filter_applied'):
             operation_list = self.get_queryset()
-            paginator = Paginator(operation_list, 100)
+
+            paginator = Paginator(operation_list, 50)
             page_number = request.GET.get('page')
             page_obj = paginator.get_page(page_number)
-            print(page_obj)
-            return render(request, 'operations/operation_list.html', {'operation_list': operation_list,
+            operation_list_page = paginator.page(page_number).object_list if True else ''
+
+            return render(request, 'operations/operation_list.html', {'operation_list': operation_list_page,
                                                                       'page_obj': page_obj})
         return super().get(request, *args, **kwargs)
 
@@ -70,9 +111,27 @@ def get_row_and_id(model, column):
     array = {}
     if model == 'MerchantCode':
         objects = c_models.MerchantCode.objects
+    if model == 'Category':
+        objects = c_models.Category.objects
+    if model == 'Type':
+        objects = c_models.Type.objects
 
     for result in list(objects.values(column, 'id').distinct()):
         array[result['id']] = result[column]
 
     return array
 
+
+def get_foreign_objects_by_filter(model, filter_args):
+    # print(filter_args)
+    objects = None
+    array = []
+    if model == 'MerchantCode':
+        objects = c_models.MerchantCode.objects
+    if model == 'Category':
+        objects = c_models.Category.objects
+
+    for result in list(objects.filter(**filter_args).values('id').distinct()):
+        array.append(result['id'])
+
+    return array
--- static/js/operations.js
+++ static/js/operations.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
     $('.operationListFilter').on('change', sendFilter);
+    $('.operationListPage').on('click', sendFilter);
 });
 
 function sendFilter() {
-    let status = collectSelectedValues('status');
-    let merchant_code = collectSelectedValues('merchant_code');
+    let status = $('#filter_status').val();
+    let merchant_code = $('#filter_merchant_code').val();
+    let category = $('#filter_category').val();
+    let type = $('#filter_type').val();
+    let date_from = $('#filter_date_from')[0].value;
+    let date_to = $('#filter_date_to')[0].value;
+    let page_number  = findPageNumber(this);
+    let orderby = $('#filter_orderby')[0].value;
 
     $.ajax({
         url: "",
@@ -13,19 +20,22 @@ function sendFilter() {
         data: {
             filter_applied: 'True',
             status: status,
-            merchant_code: merchant_code
+            date_from: date_from,
+            date_to: date_to,
+            merchant_code: merchant_code,
+            category: category,
+            type: type,
+            page: page_number,
+            orderby: orderby
         },
         complete: function (data) {
             $('.operationsListTableContainer').html(data.responseText);
+            $('.operationListPage').on('click', sendFilter);
         }
     });
 }
 
-function collectSelectedValues(filter) {
-    let arr = [];
-    $('#filter_'+filter+' option:selected').each(function() {
-        arr.push(this.value);
-    });
-
-    return arr;
+function findPageNumber(object) {
+    let currentPage = $('.currentListPage').length > 0 ? $('.currentListPage').attr('id').slice(5) : '1';
+    return object.classList.contains('operationListPage') ? object.id.slice(5) : currentPage;
 }
