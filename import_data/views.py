ae8bc04172efa5ba88e5b599b6a3b2450892177e
rostoves <rostoves@users.noreply.github.com>
2020-05-05 13:40:34 +0300

    Regular operations planning

--- import_data/views.py
+++ import_data/views.py
@@ -1,10 +1,12 @@
 import logging
 import json
-from datetime import datetime
-from pandas import read_csv
+from datetime import datetime, timedelta
+from pandas import read_csv, date_range
 from django.shortcuts import render
 from django.views.generic import FormView
 from django.http import JsonResponse
+from django.db.models import Sum, Max
+from django.db.models.functions import TruncDate
 
 from . import forms
 from categories import models as c_models
@@ -17,27 +19,51 @@ logger = logging.getLogger('django')
 class ImportCsvView(FormView):
     form_class = forms.ImportFileForm
     template_name = 'import_data/import_csv.html'
+    plan_desc_id = 2998
 
     def post(self, request, *args, **kwargs):
         if request.is_ajax():
             if request.POST['action'] == 'get_mcc':
                 return JsonResponse(get_column('MerchantCode', 'name'), safe=False)
             if request.POST['action'] == 'import_table':
-                non_inserted = ImportTable(request.POST['data']).insert()
-                if non_inserted is not None:
-                    return JsonResponse(non_inserted, safe=False)
-                else:
-                    return JsonResponse({'ok': 'ok'})
+                insert_result = ImportTable(request.POST['data']).insert()
+                delete_object('Operation', {'description_id__exact': self.plan_desc_id,
+                                            'date__lte': datetime.today() + timedelta(days=1)})
+
+                UpdateRegularPlans(self.plan_desc_id).update_regular_operations()
+                return JsonResponse(insert_result, safe=False)
         else:
             form_class = self.get_form_class()
             form = self.get_form(form_class)
             file = request.FILES['file']
             if form.is_valid():
-                table = parse_csv(file)
-                table = find_mcc_for_desc(table)
+                table = self.parse_csv(file)
+                table = self.find_mcc_for_desc(table)
                 return render(request, 'import_data/import_table.html', {'table': table})
             else:
                 return self.form_invalid(form)
+
+    @staticmethod
+    def parse_csv(csv_file):
+        table = read_csv(csv_file, encoding='cp1251', delimiter=';', keep_default_na=False, header=0,
+                         decimal=",",
+                         names=['date', 'bargain_date', 'account', 'status', 'operation_sum', 'operation_cur',
+                                'bargain_sum', 'bargain_cur', 'cashback', 'merchant_code', 'mcc', 'description',
+                                'bonuses'])
+
+        table = table.iloc[::-1].T.to_dict()
+        return table.values
+
+    @staticmethod
+    def find_mcc_for_desc(table):
+        for row in table():
+            description = c_models.Description.objects.get(name=row['description'])
+            if description.merchant_code:
+                row['merchant_code_matched'] = description.merchant_code
+            else:
+                row['merchant_code_matched'] = row['merchant_code']
+
+        return table
 
 
 class ImportTable(object):
@@ -122,25 +148,97 @@ class ImportTable(object):
             return {'result': 0, 'id': _item['id']}
 
 
-def parse_csv(csv_file):
-    table = read_csv(csv_file, encoding='cp1251',  delimiter=';', keep_default_na=False, header=0, decimal=",",
-                     names=['date', 'bargain_date', 'account', 'status', 'operation_sum', 'operation_cur',
-                            'bargain_sum', 'bargain_cur', 'cashback', 'merchant_code', 'mcc', 'description',
-                            'bonuses'])
+class UpdateRegularPlans(object):
+    model = o_models.Operation
+    today = datetime.today()
+    today = datetime.date(today)
 
-    table = table.iloc[::-1].T.to_dict()
-    return table.values
+    def __init__(self, regular_desc_id):
+        self.plan_desc_id = regular_desc_id
+        self.period_avg_calc = 180
+        self.plan_period = 364
+        self.regular_type_id = 2
+        self.plan_status = 'PLAN'
+        self.plan_cur = 1079
+        self.plan_account = 24
+        self.regulars = self.get_queryset()
+
+    def get_queryset(self):
+        result = self.model.objects.filter(merchant_code__category__type=self.regular_type_id,
+                                           date__date__gte=self.today-timedelta(days=self.period_avg_calc))\
+            .exclude(description_id__exact=self.plan_desc_id).values('merchant_code', 'merchant_code__name')\
+            .annotate(bargain_sum=Sum('bargain_sum'))
+
+        for entry in result:
+            entry['bargain_sum'] = round(entry['bargain_sum']/self.period_avg_calc, 2)
+            logger.info("Average sum for "+entry['merchant_code__name']+" operations: "+str(entry['bargain_sum']))
+
+        return result
+
+    def update_regular_operations(self):
+        current_planned_date = self.model.objects.filter(description_id__exact=self.plan_desc_id).aggregate(date=TruncDate(Max('date'), tzinfo='Europe/Moscow'))['date']
+        logger.info("Latest date with planned operations: " + str(current_planned_date))
+        period_for_planning = self.plan_period - (current_planned_date - self.today).days
+
+        date_list = date_range(current_planned_date, periods=period_for_planning, normalize=True).tolist()
+        for date in date_list:
+            logger.info("New date for planning: : " + str(date))
+        for entry in self.regulars:
+            update_object('Operation', {'merchant_code_id': entry['merchant_code'], 'description_id': self.plan_desc_id},
+                          {'bargain_sum': entry['bargain_sum']})
+
+            for date in date_list:
+                result = self.model.objects.update_or_create(merchant_code_id__exact=entry['merchant_code'],
+                                                             description_id__exact=self.plan_desc_id,
+                                                             date__exact=date,
+                                                             defaults={
+                                                                'date': date,
+                                                                'account_id': self.plan_account,
+                                                                'status': self.plan_status,
+                                                                'operation_sum': entry['bargain_sum'],
+                                                                'operation_cur_id': self.plan_cur,
+                                                                'bargain_sum': entry['bargain_sum'],
+                                                                'bargain_cur_id': self.plan_cur,
+                                                                'merchant_code_id': entry['merchant_code'],
+                                                                'description_id': self.plan_desc_id
+                                                             })
+                if result[1]:
+                    logger.info("New operation planned: : " + str(result[0]))
+                else:
+                    logger.info("Operation already planned: : " + str(result[0]))
 
 
-def find_mcc_for_desc(table):
-    for row in table():
-        description = c_models.Description.objects.get(name=row['description'])
-        if description.merchant_code:
-            row['merchant_code_matched'] = description.merchant_code
-        else:
-            row['merchant_code_matched'] = row['merchant_code']
+def update_object(model, filters, updates):
+    logger.info('Requested update for ' + model)
+    logger.info('Objects to update: ')
+    for k, v in filters.items():
+        logger.info('where ' + k + ' = ' + str(v))
+    for k, v in updates.items():
+        logger.info('Field to update: ' + k + ' to ' + str(v))
+    objects = None
+    if model == 'MerchantCode':
+        objects = c_models.MerchantCode.objects
+    if model == 'Operation':
+        objects = o_models.Operation.objects
 
-    return table
+    result = objects.filter(**filters).update(**updates)
+    logger.info('Updated number of objects: ' + str(result))
+    return result
+
+
+def delete_object(model, filters):
+    logger.warning('Requested delete from ' + model)
+    logger.warning('Objects to delete: ')
+    for k, v in filters.items():
+        logger.warning('where ' + k + ' = ' + str(v))
+    objects = None
+    if model == 'Operation':
+        objects = o_models.Operation.objects
+
+    result = objects.filter(**filters).delete()[1]
+    for k, v in result.items():
+        logger.warning('Deleted from ' + k + '. Number of items: ' + str(v))
+    return result
 
 
 def get_column(model, column):
@@ -151,6 +249,8 @@ def get_column(model, column):
         objects = c_models.Description.objects
     if model == 'MerchantCode':
         objects = c_models.MerchantCode.objects
+    if model == 'Operation':
+        objects = o_models.Operation.objects
 
     for result in list(objects.values(column).distinct()):
         array.append(result[column])
